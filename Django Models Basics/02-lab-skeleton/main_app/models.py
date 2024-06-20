59d6ff7e0a8e2f9de8918561ceb832f599432efd
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-20 21:17:13 +0300

    Initial Commit

--- Django Models Basics/02-lab-skeleton/main_app/models.py
+++ Django Models Basics/02-lab-skeleton/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 
@@ -46,3 +48,42 @@ class Department(models.Model):
         editable=False
     )
 
+class Project(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    description = models.TextField(
+        null=True,
+        blank=True
+    )
+    budget = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=10,
+        decimal_places=2
+    )
+    duration_in_days = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Duration in Days',
+    )
+    estimated_hours = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name='Estimated Hours',
+    )
+    start_date = models.DateField(
+        verbose_name='Start Date',
+        null=True,
+        blank=True,
+        default=date.today()
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+    last_edited_on = models.DateTimeField(
+        auto_now=True,
+        editable=False
+    )
