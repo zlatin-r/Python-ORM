705ed5fcb1c8ef4899de101bc0229b15d83b2e22
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-20 20:29:36 +0300

    Initial Commit

--- Django Models Basics/02-lab-skeleton/orm_skeleton/settings.py
+++ Django Models Basics/02-lab-skeleton/orm_skeleton/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
--- Django Models Basics/02-lab-skeleton/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=20)
-    age = models.IntegerField()
-    gender = models.CharField(max_length=20)
-    email = models.EmailField()
