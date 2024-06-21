7358b65eae562bf2afc81f0d4725add080268d38
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-21 09:03:45 +0200

    commit

--- Django Models Basics - Exercise/02-exercise-skeleton/main_app/models.py
+++ Django Models Basics - Exercise/02-exercise-skeleton/main_app/models.py
@@ -35,3 +35,17 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True
     )
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=70
+    )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
