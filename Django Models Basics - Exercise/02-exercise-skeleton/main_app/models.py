474b8df8f17506adc5529edfee38492f037dde05
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-21 09:01:15 +0200

    commit

--- Django Models Basics - Exercise/02-exercise-skeleton/main_app/models.py
+++ Django Models Basics - Exercise/02-exercise-skeleton/main_app/models.py
@@ -10,8 +10,28 @@ class Person(models.Model):
     age = models.PositiveIntegerField()
 
 
+class Blog(models.Model):
+    post = models.TextField()
+    author = models.CharField(
+        max_length=35
+    )
+
+
 class WeatherForecast(models.Model):
     date = models.DateField()
     temperature = models.FloatField()
     humidity = models.FloatField()
-    pressure = models.FloatField()
+    precipitation = models.FloatField()
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    description = models.TextField()
+    ingredients = models.TextField()
+    cook_time = models.PositiveIntegerField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
