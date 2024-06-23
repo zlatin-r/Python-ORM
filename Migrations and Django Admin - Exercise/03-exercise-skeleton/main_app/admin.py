22c0384cc028bd1f13d148de6f52a3a177fdd25b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 16:37:10 +0200

    Initial Commit

--- Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/admin.py
+++ Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from main_app.models import EventRegistration
+
+
 # Register your models here.
+
+
+@admin.register(EventRegistration)
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('event_name', 'participant_name', 'registration_date')
+    list_filter = ('event_name', 'registration_date')
+    search_fields = ('event_name', 'participant_name')
