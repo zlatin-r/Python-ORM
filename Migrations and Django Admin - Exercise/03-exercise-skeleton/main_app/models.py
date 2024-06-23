e51ac5f8f27f97d263b38f50bce6601cf5a1a152
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 17:29:20 +0200

    Commit

--- Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
+++ Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
@@ -46,8 +46,20 @@ class Supplier(models.Model):
     name = models.CharField(max_length=100)
     contact_person = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20, unique=True)
     address = models.TextField()
 
     def __str__(self):
-        return f"{self.name} - {self.phone} "
+        return f"{self.name} - {self.phone}"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=90)
+    lecturer = models.CharField(max_length=90)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.lecturer}"
