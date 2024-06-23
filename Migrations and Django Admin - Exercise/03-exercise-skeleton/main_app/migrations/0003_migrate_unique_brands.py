e46239c628c9eb3635210706a1d366c8350b6974
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 16:29:50 +0200

    Initial Commit

--- Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/migrations/0003_migrate_unique_brands.py
+++ Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/migrations/0003_migrate_unique_brands.py
@@ -3,11 +3,23 @@
 from django.db import migrations
 
 
-class Migration(migrations.Migration):
+def create_unique_brands(apps, schema_editor):
+    shoe = apps.get_model('main_app', 'Shoe')
+    unique_brands = apps.get_model('main_app', 'UniqueBrands')
 
+    db_alias = schema_editor.connection.alias
+
+    unique_brand_names = shoe.objects.values_list('brand', flat=True).distinct()
+
+    for brand_name in unique_brand_names:
+        unique_brands.objects.using(db_alias).create(brand_name=brand_name)
+
+
+class Migration(migrations.Migration):
     dependencies = [
         ('main_app', '0002_uniquebrands'),
     ]
 
     operations = [
+        migrations.RunPython(create_unique_brands)
     ]
