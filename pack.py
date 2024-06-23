e74630be2d77d3a882d858ec7d49111b876deb9b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 09:53:25 +0100

    Initial Commit

--- pack.py
+++ pack.py
@@ -1,29 +1,20 @@
-import datetime
 import os
 import zipfile
-
-
+import datetime
 def pack():
-
     dt = datetime.datetime.now().strftime('%H-%M_%d.%m.%y')
-
     with zipfile.ZipFile(f'submission-{dt}.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(os.getcwd()):
             for file in files:
                 file_path = os.path.join(root, file)
                 archive_path = os.path.relpath(file_path, '.')
-                current_dir = os.path.basename(root)
-
-                # Skip files in the venv directory
-                if 'venv' in root.split(os.path.sep):
-                    continue
-
-                if file in ['requirements.txt', 'manage.py', 'caller.py'] or current_dir in ['main_app', 'orm_skeleton',
-                                                                                             'migrations']:
+                current_dir = root.split('\\')[-1]
+                if file in ['requirements.txt', 'manage.py', 'caller.py']\
+                        or current_dir in ['main_app', 'orm_skeleton', 'migrations']:
                     zipf.write(file_path, archive_path)
 
     print('Submission created!')
-
-
 if __name__ == '__main__':
     pack()
+
+
