61cd04a95a180e35d8b4c6d34763277b402ca1e7
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 10:06:02 +0100

    Initial Commit

--- Migrations and Django Admin/03-lab-skeleton/main_app/admin.py
+++ Migrations and Django Admin/03-lab-skeleton/main_app/admin.py
@@ -4,4 +4,15 @@ from main_app.models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'category', 'price', 'created_on')
+    search_fields = ('name', 'category', 'supplier')
+    list_filter = ('category', 'supplier')
+    fieldsets = (
+        ('General Information',
+         {'fields': ('name', 'description', 'price', 'barcode')}
+         ),
+        ('Categorization',
+         {'fields': ('category', 'supplier')}
+         ),
+    )
+    date_hierarchy = 'created_on'
