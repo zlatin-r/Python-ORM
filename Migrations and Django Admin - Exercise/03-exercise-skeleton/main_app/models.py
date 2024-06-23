172d5bf92583fb8381d26a533ab5e74bdf3249aa
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 18:12:05 +0200

    Commit

--- Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
+++ Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
@@ -72,3 +72,10 @@ class Person(models.Model):
 
     def __str__(self):
         return f"Name: {self.name}"
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    rarity = models.CharField(max_length=20, default='No rarity')
