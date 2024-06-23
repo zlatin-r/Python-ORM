566d1af160760458903aad82c24901d899265261
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 15:43:54 +0200

    Initial Commit

--- Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
+++ Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class Shoe(models.Model):
     brand = models.CharField(max_length=25)
     size = models.PositiveIntegerField()
+
+
+class UniqueBrands(models.Model):
+    brand_name = models.CharField(max_length=25)
