e3beaa544374df37febc7e0734aa6333d6ae0e00
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-23 17:53:00 +0200

    Commit

--- Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
+++ Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
@@ -68,7 +68,7 @@ class Course(models.Model):
 class Person(models.Model):
     name = models.CharField(max_length=40)
     age = models.PositiveIntegerField()
-    age_group = models.CharField(max_length=20, default='No age group')
+    age_group = models.CharField(max_length=20, default="No age group")
 
     def __str__(self):
         return f"Name: {self.name}"
