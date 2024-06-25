d1f641bb906c4ab188b2f7f5b085954f9b30fae1
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-25 12:26:29 +0200

    Commit

--- Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
+++ Migrations and Django Admin - Exercise/03-exercise-skeleton/main_app/models.py
@@ -82,3 +82,9 @@ class Item(models.Model):
     rarity = models.CharField(max_length=20, default='No rarity')
 
 
+class Smartphone(models.Model):
+    brand = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    category = models.CharField(max_length=20, default='No category')
+
+
