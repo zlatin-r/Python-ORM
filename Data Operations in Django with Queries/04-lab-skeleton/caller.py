be1d15182ce6b5c98f526b500d431f6d80705386
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-27 12:11:47 +0200

    Commit

--- Data Operations in Django with Queries/04-lab-skeleton/caller.py
+++ Data Operations in Django with Queries/04-lab-skeleton/caller.py
@@ -7,5 +7,50 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Student
 
+
+def add_students():
+    student = Student(
+        student_id='FC5204',
+        first_name='John',
+        last_name='Doe',
+        birth_date='1995-05-15',
+        email='john.doe@university.com'
+    )
+    student.save()
+
+    Student.objects.create(
+        student_id='FE0054',
+        first_name='Jane',
+        last_name='Smith',
+        email='jane.smith@university.com'
+    )
+
+    student = Student()
+    student.student_id = 'FH2014'
+    student.first_name = 'Alice'
+    student.last_name = 'Johnson'
+    student.birth_date = '1998-02-10'
+    student.email = 'alice.johnson@university.com'
+    student.save()
+
+    Student.objects.create(
+        student_id='FH2015',
+        first_name='Bob',
+        last_name='Wilson',
+        birth_date='1996-11-25',
+        email='bob.wilson@university.com'
+    )
+
+
+'''
+FC5204	John	Doe	15/05/1995	john.doe@university.com
+FE0054	Jane	Smith	null	jane.smith@university.com
+FH2014	Alice	Johnson	10/02/1998	alice.johnson@university.com
+FH2015	Bob	Wilson	25/11/1996	bob.wilson@university.com
+
+'''
 # Run and print your queries
+add_students()
+print(Student.objects.all())
