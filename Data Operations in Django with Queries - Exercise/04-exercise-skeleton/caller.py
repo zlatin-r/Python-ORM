64dc2233b496a3a35cdc6ff2f85a80dd3b996c42
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-28 18:31:00 +0200

    Commit

--- Data Operations in Django with Queries - Exercise/04-exercise-skeleton/caller.py
+++ Data Operations in Django with Queries - Exercise/04-exercise-skeleton/caller.py
@@ -131,19 +131,30 @@ def get_deluxe_rooms() -> str:
 def increase_room_capacity() -> None:
     rooms = HotelRoom.objects.all().order_by('id')
 
-    for i in range(len(rooms)):
-        if not rooms[i].is_reserved:
+    previous_room_capacity = None
+
+    for room in rooms:
+        if not room.is_reserved:
             continue
-        if i == 0 or len(rooms) == 1:
-            rooms[i].capacity += rooms[i].id
+
+        if previous_room_capacity is not None:
+            room.capacity += previous_room_capacity
         else:
-            if not rooms[i - 1].is_reserved and rooms[i].is_reserved:
-                rooms[i].capacity += rooms[i - 1].capacity
+            room.capacity += room.id
+
+        previous_room_capacity = room.capacity
+
+    HotelRoom.objects.bulk_update(rooms, ['capacity'])
 
 
 def reserve_first_room() -> None:
-    HotelRoom.objects.first().reserved = True
+    room = HotelRoom.objects.first()
+    room.is_reserved = True
+    room.save()
 
 
 def delete_last_room() -> None:
-    HotelRoom.objects.last().delete()
+    room = HotelRoom.objects.last()
+
+    if not room.is_reserved:
+        room.delete()
