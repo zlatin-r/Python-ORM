ef65de8c38ebf9ff58f9a1140de920fd5b72aee8
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-28 14:54:07 +0200

    Commit

--- Data Operations in Django with Queries - Exercise/04-exercise-skeleton/caller.py
+++ Data Operations in Django with Queries - Exercise/04-exercise-skeleton/caller.py
@@ -37,29 +37,10 @@ def rename_artifact(artifact: Artifact, new_name: str):
         artifact.name = new_name
         artifact.save()
 
+    # Artifact.objects.filter(is_magical=True, age__gt=250, pk=artifact.pk).update(name=new_name)
+
 
 def delete_all_artifacts():
-    all_artifacts = Artifact.objects.all()
-    for artifact in all_artifacts:
-        artifact.delete()
+    Artifact.objects.all().delete()
 
 
-# create_artifact('Ancient Sword',
-#                 'Lost Kingdom',
-#                 500,
-#                 'A legendary sword with a rich history', True)
-# create_artifact('Crystal Amulet',
-#                 'Mystic Forest',
-#                 300,
-#                 'A magical amulet believed to bring good fortune', True
-#                 )
-# create_artifact('Stone Tablet',
-#                 'Ruined Temple',
-#                 1000,
-#                 'An ancient tablet covered in mysterious inscriptions', False
-#                 )
-#
-# print(create_artifact('Ancient Sword', 'Lost Kingdom', 500, 'A legendary sword with a rich history', True))
-# artifact_object = Artifact.objects.get(name='Ancient Sword')
-# rename_artifact(artifact_object, 'Ancient Shield')
-# print(artifact_object.name)
