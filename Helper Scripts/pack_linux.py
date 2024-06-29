817f40ba2edb1ebd2169f478fe886648dc619052
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-29 14:28:22 +0200

    Commit

--- Helper Scripts/pack_linux.py
+++ Helper Scripts/pack_linux.py
@@ -2,23 +2,23 @@ import os
 import zipfile
 import datetime
 
-
 def pack():
+
     dt = datetime.datetime.now().strftime('%H-%M_%d.%m.%y')
-    with zipfile.ZipFile(f'submission-{dt}.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    archive_name = f'submission-{dt}.zip'
+
+    with zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(os.getcwd()):
-            if '.venv' or 'venv' in root.split(os.sep):
-                continue
             for file in files:
                 file_path = os.path.join(root, file)
-                archive_path = os.path.relpath(file_path, '.')
-                current_dir = root.split('/')[-1]
-                if file in ['requirements.txt', 'manage.py', 'caller.py'] or current_dir in ['main_app', 'orm_skeleton',
-                                                                                             'migrations']:
+                archive_path = os.path.relpath(file_path, os.getcwd())
+                current_dir = os.path.basename(root)
+                
+                if file in ['requirements.txt', 'manage.py', 'caller.py'] or current_dir in ['main_app', 'orm_skeleton', 'migrations']:
                     zipf.write(file_path, archive_path)
 
-    print('Submission created!')
-
+    print(f'Submission created: {archive_name}')
 
 if __name__ == '__main__':
     pack()
+
