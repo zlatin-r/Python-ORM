2bb68915169ed21d12881b8446950a0adb43e983
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-30 10:54:09 +0200

    Commit

--- Working with queries in Django - Exercise/05-exercise-skeleton/caller.py
+++ Working with queries in Django - Exercise/05-exercise-skeleton/caller.py
@@ -1,11 +1,15 @@
 import os
+from typing import List
+
 import django
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import ArtworkGallery
+from main_app.models import ArtworkGallery, Laptop
+
+
 # from populate_db import populate_model_with_data
 # populate_model_with_data(ArtworkGallery, 10)
 
@@ -23,6 +27,89 @@ def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> N
 
 
 def delete_negative_rated_arts() -> None:
-    delete_artworks = ArtworkGallery.objects.filter(rating__lt=0).delete()
+    ArtworkGallery.objects.filter(rating__lt=0).delete()
 
 
+def show_the_most_expensive_laptop() -> str:
+    most_expensive = Laptop.objects.order_by('-price').first()
+
+    return f"{most_expensive.brand} is the most expensive laptop available for {most_expensive.price}$!"
+
+
+def bulk_create_laptops(args: List[Laptop]) -> None:
+    Laptop.objects.bulk_create(args)
+
+
+def update_to_512_GB_storage() -> None:
+    Laptop.objects.filter(brand__in=['Asus', 'Lenovo']).update(storage=512)
+
+
+def update_to_16_GB_memory() -> None:
+    Laptop.objects.filter(brand__in=['Apple', 'Dell']).update(storage=16)
+
+
+def update_operation_systems() -> None:
+    all_laptops = Laptop.objects.all()
+
+    for laptop in all_laptops:
+        if laptop.brand == "Asus":
+            laptop.operation_system = "Windows"
+        elif laptop.brand == "Apple":
+            laptop.operation_system = "MacOS"
+        elif laptop.brand in ["Dell", "Acer"]:
+            laptop.operation_system = "Linux"
+        elif laptop.brand == "Lenovo":
+            laptop.operation_system = "Chrome OS"
+
+        laptop.save()
+
+    # Laptop.objects.filter(brand='Asus').update(operating_system='Windows')
+    # Laptop.objects.filter(brand='Apple').update(operating_system='MacOS')
+    # Laptop.objects.filter(brand__in=['Dell', 'Acer']).update(operating_system='Linux')
+    # Laptop.objects.filter(brand='Lenovo').update(operating_system='Chrome OS')
+
+
+def delete_inexpensive_laptops() -> None:
+    Laptop.objects.filter(price__lt=1200).delete()
+
+
+laptop1 = Laptop(
+    brand='Asus',
+    processor='Intel Core i5',
+    memory=8,
+    storage=256,
+    operation_system='MacOS',
+    price=899.99
+)
+laptop2 = Laptop(
+    brand='Apple',
+    processor='Chrome OS',
+    memory=16,
+    storage=256,
+    operation_system='MacOS',
+    price=1399.99
+)
+laptop3 = Laptop(
+    brand='Lenovo',
+    processor='AMD Ryzen 7',
+    memory=12,
+    storage=256,
+    operation_system='Linux',
+    price=999.99,
+)
+
+# Create a list of instances
+laptops_to_create = [laptop1, laptop2, laptop3]
+
+# Use bulk_create to save the instances
+bulk_create_laptops(laptops_to_create)
+
+update_to_512_GB_storage()
+update_operation_systems()
+
+# Retrieve 2 laptops from the database
+asus_laptop = Laptop.objects.filter(brand__exact='Asus').get()
+lenovo_laptop = Laptop.objects.filter(brand__exact='Lenovo').get()
+
+print(asus_laptop.storage)
+print(lenovo_laptop.operation_system)
--- Working with queries in Django - Exercise/05-exercise-skeleton/main_app/models.py
+++ Working with queries in Django - Exercise/05-exercise-skeleton/main_app/models.py
@@ -89,6 +89,6 @@ class Laptop(models.Model):
     processor = models.CharField(max_length=100)
     memory = models.PositiveIntegerField(help_text='Memory in GB')
     storage = models.PositiveIntegerField(help_text='Storage in GB')
-    operating_system = models.CharField(choices=OperatingSystem.choices)
+    operation_system = models.CharField(choices=OperatingSystem.choices)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
