48a3df9c7e4a3e23e7848178eb8a72b802c0bc36
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-30 16:07:59 +0200

    Commit

--- Working with queries in Django - Exercise/05-exercise/caller.py
+++ Working with queries in Django - Exercise/05-exercise/caller.py
@@ -5,6 +5,20 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models
-# Create and check models
-# Run and print your queries
+from main_app.models import ArtworkGallery
+
+
+def show_highest_rated_art() -> str:
+    highest_rated = ArtworkGallery.objects.order_by('-rating', 'id').first()
+
+    return f"{highest_rated.art_name} is the highest-rated art with a {highest_rated.rating} rating!"
+
+
+def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> None:
+    artworks = [first_art, second_art]
+
+    ArtworkGallery.objects.bulk_create(artworks)
+
+
+def delete_negative_rated_arts() -> None:
+    ArtworkGallery.objects.filter(rating__lt=0).delete()
