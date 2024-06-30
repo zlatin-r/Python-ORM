4352841e383e7114e19d52cf38e1704910593b26
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-30 11:46:45 +0200

    Commit

--- Working with queries in Django - Exercise/05-exercise-skeleton/caller.py
+++ Working with queries in Django - Exercise/05-exercise-skeleton/caller.py
@@ -7,7 +7,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import ArtworkGallery, Laptop
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer
 
 
 def show_highest_rated_art() -> str:
@@ -24,6 +24,21 @@ def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> N
 
 def delete_negative_rated_arts() -> None:
     ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
+# artwork1 = ArtworkGallery(artist_name='Vincent van Gogh',
+#                           art_name='Starry Night',
+#                           rating=4,
+#                           price=1200000.0)
+# artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci',
+#                           art_name='Mona Lisa',
+#                           rating=5,
+#                           price=1500000.0)
+#
+# # Bulk saves the instances
+# bulk_create_arts(artwork1, artwork2)
+# print(show_highest_rated_art())
+# print(ArtworkGallery.objects.all())
 
 
 def show_the_most_expensive_laptop() -> str:
@@ -69,3 +84,102 @@ def delete_inexpensive_laptops() -> None:
     Laptop.objects.filter(price__lt=1200).delete()
 
 
+# laptop1 = Laptop(
+#     brand='Asus',
+#     processor='Intel Core i5',
+#     memory=8,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=899.99
+# )
+# laptop2 = Laptop(
+#     brand='Apple',
+#     processor='Chrome OS',
+#     memory=16,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=1399.99
+# )
+# laptop3 = Laptop(
+#     brand='Lenovo',
+#     processor='AMD Ryzen 7',
+#     memory=12,
+#     storage=256,
+#     operation_system='Linux',
+#     price=999.99,
+# )
+#
+# # Create a list of instances
+# laptops_to_create = [laptop1, laptop2, laptop3]
+#
+# # Use bulk_create to save the instances
+# bulk_create_laptops(laptops_to_create)
+#
+# update_to_512_GB_storage()
+# update_operation_systems()
+#
+# # Retrieve 2 laptops from the database
+# asus_laptop = Laptop.objects.filter(brand__exact='Asus').get()
+# lenovo_laptop = Laptop.objects.filter(brand__exact='Lenovo').get()
+#
+# print(asus_laptop.storage)
+# print(lenovo_laptop.operation_system)
+
+
+def bulk_create_chess_players(args: List[ChessPlayer]) -> None:
+    ChessPlayer.objects.bulk_create(args)
+
+
+def delete_chess_players() -> None:
+    ChessPlayer.objects.filter(title='no title').delete()
+
+
+def change_chess_games_won() -> None:
+    ChessPlayer.objects.filter(title='GM').update(games_won=30)
+
+
+def change_chess_games_lost() -> None:
+    ChessPlayer.objects.filter(title='no title').update(games_lost=25)
+
+
+def change_chess_games_drawn() -> None:
+    ChessPlayer.objects.all().update(games_drawn=10)
+
+
+def grand_chess_title_GM() -> None:
+    ChessPlayer.objects.filter(rating__gte=2400).update(title='GM')
+
+
+def grand_chess_title_IM() -> None:
+    ChessPlayer.objects.filter(rating__range=[2300, 2399]).update(title='IM')
+
+
+def grand_chess_title_FM() -> None:
+    ChessPlayer.objects.filter(rating__range=[2200, 2299]).update(title='FM')
+
+
+def grand_chess_title_regular_player() -> None:
+    ChessPlayer.objects.filter(rating__range=[0, 2199]).update(title='regular player')
+
+
+player1 = ChessPlayer(username='Player1',
+                      title='no title',
+                      rating=2200,
+                      games_played=50,
+                      games_won=20,
+                      games_lost=25,
+                      games_drawn=5, )
+player2 = ChessPlayer(username='Player2',
+                      title='IM',
+                      rating=2350,
+                      games_played=80,
+                      games_won=40,
+                      games_lost=25,
+                      games_drawn=15, )
+
+# # Call the bulk_create_chess_players function
+# bulk_create_chess_players([player1, player2])
+# # Call the delete_chess_players function
+# delete_chess_players()
+# # Check that the players are deleted
+# print("Number of Chess Players after deletion:", ChessPlayer.objects.count())
