21c848480e712466ac86fe7ec643589740b74f93
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-06-30 16:10:21 +0200

    Commit

--- Working with queries in Django - Exercise/05-exercise/caller.py
+++ Working with queries in Django - Exercise/05-exercise/caller.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import ArtworkGallery
+from main_app.models import ArtworkGallery, Laptop
 
 
 def show_highest_rated_art() -> str:
@@ -22,3 +22,37 @@ def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> N
 
 def delete_negative_rated_arts() -> None:
     ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
+def show_the_most_expensive_laptop() -> str:
+    most_expensive = Laptop.objects.order_by('-price', '-id').first()
+
+    return f"{most_expensive.brand} is the most expensive laptop available for {most_expensive.price}$!"
+
+
+def bulk_create_laptops(args: List[Laptop]) -> None:
+    Laptop.objects.bulk_create(args)
+
+
+def update_to_512_GB_storage() -> None:
+    Laptop.objects.filter(brand__in=['Asus', 'Lenovo']).update(storage=512)
+
+
+def update_to_16_GB_memory() -> None:
+    Laptop.objects.filter(brand__in=['Apple', 'Dell']).update(memory=16)
+
+
+def update_operation_systems() -> None:
+    all_laptops = Laptop.objects.all()
+
+    for laptop in all_laptops:
+        if laptop.brand == "Asus":
+            laptop.operation_system = "Windows"
+        elif laptop.brand == "Apple":
+            laptop.operation_system = "MacOS"
+        elif laptop.brand in ["Dell", "Acer"]:
+            laptop.operation_system = "Linux"
+        elif laptop.brand == "Lenovo":
+            laptop.operation_system = "Chrome OS"
+
+        laptop.save()
