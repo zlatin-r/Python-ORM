7c8a83498da81ae47741cb29c259954f67689488
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-01 14:25:00 +0200

    Commit

--- Working with queries in Django - Exercise/05-exercise/caller.py
+++ Working with queries in Django - Exercise/05-exercise/caller.py
@@ -20,74 +20,7 @@ from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal, Dungeon, 
 
 
 
-def show_hard_dungeons() -> str:
-    hard_dungeons = Dungeon.objects.filter(difficulty='Hard').order_by('-location')
-
-    result = [f"{d.name} is guarded by {d.boss_name} who has {d.boss_health} health points!"
-              for d in hard_dungeons]
-
-    return '\n'.join(result)
 
 
-def bulk_create_dungeons(args: List[Dungeon]) -> None:
-    Dungeon.objects.bulk_create(args)
 
 
-def update_dungeon_names() -> None:
-    Dungeon.objects.filter(difficulty='Easy').update(name='The Erased Thombs')
-    Dungeon.objects.filter(difficulty='Medium').update(name='The Coral Labyrinth')
-    Dungeon.objects.filter(difficulty='Hard').update(name='The Lost Haunt')
-
-
-def update_dungeon_bosses_health() -> None:
-    Dungeon.objects.exclude(difficulty='Easy').update(boss_health=500)
-
-
-def update_dungeon_recommended_levels() -> None:
-    Dungeon.objects.filter(difficulty='Easy').update(recommended_level=25)
-    Dungeon.objects.filter(difficulty='Medium').update(recommended_level=50)
-    Dungeon.objects.filter(difficulty='Hard').update(recommended_level=75)
-
-
-def update_dungeon_rewards() -> None:
-    Dungeon.objects.filter(boss_health=500).update(reward="1000 Gold")
-    Dungeon.objects.filter(location__startswith='E').update(reward='New dungeon unlocked')
-    Dungeon.objects.filter(location__endswith='s').update(reward='Dragonheart Amulet')
-
-
-def set_new_locations() -> None:
-    Dungeon.objects.filter(recommended_level=25).update(location='Enchanted Maze')
-    Dungeon.objects.filter(recommended_level=50).update(location='Grimstone Mines')
-    Dungeon.objects.filter(recommended_level=75).update(location='Shadowed Abyss')
-
-
-def show_workouts() -> str:
-    workouts = Workout.objects.filter(workout_type__in=["Calisthenics", "CrossFit"]).order_by("id")
-
-    result = [f"{w.name} from {w.workout_type} type has {w.difficulty} difficulty!" for w in workouts]
-
-    return '\n'.join(result)
-
-
-def get_high_difficulty_cardio_workouts() -> Workout:
-    return Workout.objects.filter(workout_type="Cardio", difficulty="High").order_by("instructor")
-
-
-def set_new_instructors() -> None:
-    Workout.objects.filter(workout_type="Cardio").update(instructor="John Smith")
-    Workout.objects.filter(workout_type="Strength").update(instructor="Michael Williams")
-    Workout.objects.filter(workout_type="Yoga").update(instructor="Emily Johnson")
-    Workout.objects.filter(workout_type="CrossFit").update(instructor="Sarah Davis")
-    Workout.objects.filter(workout_type="Calisthenics").update(instructor="Chris Heria")
-
-
-def set_new_duration_times() -> None:
-    Workout.objects.filter(instructor="John Smith").update(duration="15 minutes")
-    Workout.objects.filter(instructor="Sarah Davis").update(duration="30 minutes")
-    Workout.objects.filter(instructor="Chris Heria").update(duration="45 minutes")
-    Workout.objects.filter(instructor="Michael Williams").update(duration="1 hour")
-    Workout.objects.filter(instructor="Emily Johnson").update(duration="1 hour and 30 minutes")
-
-
-def delete_workouts() -> None:
-    Workout.objects.exclude(workout_type__in=["Strength", "Calisthenics"]).delete()
