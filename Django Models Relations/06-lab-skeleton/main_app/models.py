db37e3605d0ec8e61107e16f6718e624571ada1d
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-03 12:22:38 +0200

    Commit

--- Django Models Relations/06-lab-skeleton/main_app/models.py
+++ Django Models Relations/06-lab-skeleton/main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    lecturer = models.ForeignKey(
+        Lecturer,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
