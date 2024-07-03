43e2b97bd30e2afe186fb891cdfc4e8f748ed0b3
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-03 12:59:30 +0200

    Commit

--- Django Models Relations/06-lab-skeleton/main_app/models.py
+++ Django Models Relations/06-lab-skeleton/main_app/models.py
@@ -20,3 +20,12 @@ class Subject(models.Model):
         null=True,
         blank=True
     )
+
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    email = models.EmailField(unique=True)
+    subject = models.ManyToManyField(Subject)
