766aca72b2aa42673f9c2895c6ba98282be3cc92
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-04 09:10:29 +0200

    Commit changes

--- Django Models Relations - Exercise/06-exercise-skeleton/main_app/models.py
+++ Django Models Relations - Exercise/06-exercise-skeleton/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
