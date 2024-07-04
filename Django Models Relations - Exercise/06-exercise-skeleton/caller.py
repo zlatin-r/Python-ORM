003a1dd91856fe2ac870b08255bfb90e1c6268ce
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-04 18:06:10 +0200

    Committing more changes

--- Django Models Relations - Exercise/06-exercise-skeleton/caller.py
+++ Django Models Relations - Exercise/06-exercise-skeleton/caller.py
@@ -69,13 +69,15 @@ def add_song_to_artist(artist_name: str, song_title: str) -> None:
     artist = Artist.objects.get(name=artist_name)
     song = Song.objects.get(title=song_title)
 
-    artist.songs.add(song)
+    artist.songs.add(song)  # add(), remove(), clear()
 
 
 def get_songs_by_artist(artist_name: str) -> QuerySet:
     artist = Artist.objects.get(name=artist_name)
 
     return artist.songs.all().order_by('-id')
+
+    # Artist.objects.get(name=artist_name).songs.all().order_by('-id')
 
 
 def remove_song_from_artist(artist_name: str, song_title: str) -> None:
