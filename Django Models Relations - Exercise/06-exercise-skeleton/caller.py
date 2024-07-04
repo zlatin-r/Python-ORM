8b3f95e591ca0dcfce8b19a479bf59213b1dd542
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-04 09:28:06 +0200

    Committing more changes

--- Django Models Relations - Exercise/06-exercise-skeleton/caller.py
+++ Django Models Relations - Exercise/06-exercise-skeleton/caller.py
@@ -6,5 +6,19 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Author, Book
+
 
 # Create queries within functions
+
+def show_all_authors_with_their_books() -> str:
+    result = []
+    authors = Author.objects.all().order_by(id)
+
+    for author in authors:
+        books = author.book_set.all()
+
+        book_title = ", ".join([book.title for book in books])
+        result.append(f"{author.name} has written - {book_title}!")
+
+    return "\n".join(result)
