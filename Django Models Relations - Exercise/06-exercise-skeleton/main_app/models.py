259170de4974b9c866a24db0a8dae57380089eec
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-04 11:02:40 +0200

    Committing more changes

--- Django Models Relations - Exercise/06-exercise-skeleton/main_app/models.py
+++ Django Models Relations - Exercise/06-exercise-skeleton/main_app/models.py
@@ -21,3 +21,13 @@ class Song(models.Model):
 class Artist(models.Model):
     name = models.CharField(max_length=100, unique=True)
     songs = models.ManyToManyField(Song, related_name='artists')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Review(models.Model):
+    description = models.TextField(max_length=200)
+    rating = models.PositiveSmallIntegerField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='reviews')
