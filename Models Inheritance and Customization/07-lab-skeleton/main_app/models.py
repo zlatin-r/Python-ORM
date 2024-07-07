0c248384ec0b3bc06b876648b05bd994addec5e1
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-07 13:33:22 +0200

    Commiting more changes

--- Models Inheritance and Customization/07-lab-skeleton/main_app/models.py
+++ Models Inheritance and Customization/07-lab-skeleton/main_app/models.py
@@ -21,3 +21,28 @@ class Bird(Animal):
 
 class Reptile(Animal):
     scale_type = models.CharField(max_length=50)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+
+class SpecialityChoices(models.TextChoices):
+    MAMMALS = 'Mammals', 'Mammals'
+    BIRDS = 'Birds', 'Birds'
+    REPTILES = 'Reptiles', 'Reptiles'
+    OTHERS = 'Others', 'Others'
+
+
+class Zookeeper(Employee):
+    speciality = models.CharField(max_length=10, choices=SpecialityChoices)
+    managed_animals = models.ManyToManyField(Animal)
+
+
+class Veterinarian(Employee):
+    license_number = models.CharField(max_length=10)
