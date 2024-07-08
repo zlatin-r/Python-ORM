29f62201638af197d8bfe9b1aa28501fb800d33a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-08 09:31:42 +0200

    Commit changes

--- Models Inheritance and Customization - Exercise/07-exercise-skeleton/caller.py
+++ Models Inheritance and Customization - Exercise/07-exercise-skeleton/caller.py
@@ -6,5 +6,29 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Mage, Necromancer
 
 # Create queries within functions
+
+# Task 1:
+# # Create instances
+# mage = Mage.objects.create(
+#     name="Fire Mage",
+#     description="A powerful mage specializing in fire magic.",
+#     elemental_power="Fire",
+#     spellbook_type="Ancient Grimoire")
+#
+# necromancer = Necromancer.objects.create(
+#     name="Dark Necromancer",
+#     description="A mage specializing in dark necromancy.",
+#     elemental_power="Darkness",
+#     spellbook_type="Necronomicon",
+#     raise_dead_ability="Raise Undead Army")
+#
+# print(mage.elemental_power)
+# print(mage.spellbook_type)
+# print(necromancer.name)
+# print(necromancer.description)
+# print(necromancer.raise_dead_ability)
+# ---------------------------------------------------------------------
+# Task 2:
