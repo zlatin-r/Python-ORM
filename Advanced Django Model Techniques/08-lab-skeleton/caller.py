59c98704820c31d1e1a973d999a327e1737382ed
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-10 18:35:19 +0200

    Changes Commited

--- Advanced Django Model Techniques/08-lab-skeleton/caller.py
+++ Advanced Django Model Techniques/08-lab-skeleton/caller.py
@@ -6,65 +6,65 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Restaurant, Menu
+from main_app.models import Restaurant
 from django.core.exceptions import ValidationError
-
-# Create queries within functions
-
+#
+# # Create queries within functions
+#
 # Task 1:
-# valid_restaurant = Restaurant(
-#     name="Delicious Bistro",
-#     location="123 Main Street",
-#     description="A cozy restaurant with a variety of dishes.",
-#     rating=5.00,
+valid_restaurant = Restaurant(
+    name="Delicious Bistro",
+    location="123 Main Street",
+    description="A cozy restaurant with a variety of dishes.",
+    rating=5.00,
+)
+
+try:
+    valid_restaurant.full_clean()
+    valid_restaurant.save()
+    print("Valid Restaurant saved successfully!")
+except ValidationError as e:
+    print(f"Validation Error: {e}")
+
+invalid_restaurant = Restaurant(
+    name="A",
+    location="A" * 201,
+    description="A restaurant with a long name and invalid rating.",
+    rating=5.01,
+)
+
+try:
+    invalid_restaurant.full_clean()
+    invalid_restaurant.save()
+    print("Invalid Restaurant saved successfully!")
+except Exception as e:
+    print(f"Validation Error: {e}")
+# # -------------------------------------------------------------------------------
+# # Task 2:
+# valid_menu = Menu(
+#     name="Menu at The Delicious Bistro",
+#     description="** Appetizers: **\nSpinach and Artichoke Dip\n"
+#                 "** Main Course: **\nGrilled Salmon\n"
+#                 "** Desserts: **\nChocolate Fondue",
+#     restaurant=Restaurant.objects.first(),
 # )
 #
 # try:
-#     valid_restaurant.full_clean()
-#     valid_restaurant.save()
-#     print("Valid Restaurant saved successfully!")
+#     valid_menu.full_clean()
+#     valid_menu.save()
+#     print("Valid Menu saved successfully!")
 # except ValidationError as e:
 #     print(f"Validation Error: {e}")
 #
-# invalid_restaurant = Restaurant(
-#     name="A",
-#     location="A" * 201,
-#     description="A restaurant with a long name and invalid rating.",
-#     rating=5.01,
+# invalid_menu = Menu(
+#     name="Incomplete Menu",
+#     description="** Appetizers: **\nSpinach and Artichoke Dip",
+#     restaurant=Restaurant.objects.first(),
 # )
 #
 # try:
-#     invalid_restaurant.full_clean()
-#     invalid_restaurant.save()
-#     print("Invalid Restaurant saved successfully!")
-# except Exception as e:
+#     invalid_menu.full_clean()
+#     invalid_menu.save()
+#     print("Invalid Menu saved successfully!")
+# except ValidationError as e:
 #     print(f"Validation Error: {e}")
-# -------------------------------------------------------------------------------
-# Task 2:
-valid_menu = Menu(
-    name="Menu at The Delicious Bistro",
-    description="** Appetizers: **\nSpinach and Artichoke Dip\n** "
-                "Main Course: **\nGrilled Salmon\n** "
-                "Desserts: **\nChocolate Fondue",
-    restaurant=Restaurant.objects.first(),
-)
-
-try:
-    valid_menu.full_clean()
-    valid_menu.save()
-    print("Valid Menu saved successfully!")
-except ValidationError as e:
-    print(f"Validation Error: {e}")
-
-invalid_menu = Menu(
-    name="Incomplete Menu",
-    description="** Appetizers: **\nSpinach and Artichoke Dip",
-    restaurant=Restaurant.objects.first(),
-)
-
-try:
-    invalid_menu.full_clean()
-    invalid_menu.save()
-    print("Invalid Menu saved successfully!")
-except ValidationError as e:
-    print(f"Validation Error: {e}")
