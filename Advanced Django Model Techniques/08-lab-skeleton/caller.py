a87cc31c8429c3f6efae1f71d9b03e2588b1ef3a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-10 16:13:05 +0200

    Changes Commited

--- Advanced Django Model Techniques/08-lab-skeleton/caller.py
+++ Advanced Django Model Techniques/08-lab-skeleton/caller.py
@@ -11,31 +11,33 @@ from django.core.exceptions import ValidationError
 
 # Create queries within functions
 
-
-valid_restaurant = Restaurant(
-    name="Delicious Bistro",
-    location="123 Main Street",
-    description="A cozy restaurant with a variety of dishes.",
-    rating=5.00,
-)
-
-try:
-    valid_restaurant.full_clean()
-    valid_restaurant.save()
-    print("Valid Restaurant saved successfully!")
-except ValidationError as e:
-    print(f"Validation Error: {e}")
-
-invalid_restaurant = Restaurant(
-    name="A",
-    location="A" * 201,
-    description="A restaurant with a long name and invalid rating.",
-    rating=5.01,
-)
-
-try:
-    invalid_restaurant.full_clean()
-    invalid_restaurant.save()
-    print("Invalid Restaurant saved successfully!")
-except Exception as e:
-    print(f"Validation Error: {e}")
+# Task 1:
+# valid_restaurant = Restaurant(
+#     name="Delicious Bistro",
+#     location="123 Main Street",
+#     description="A cozy restaurant with a variety of dishes.",
+#     rating=5.00,
+# )
+#
+# try:
+#     valid_restaurant.full_clean()
+#     valid_restaurant.save()
+#     print("Valid Restaurant saved successfully!")
+# except ValidationError as e:
+#     print(f"Validation Error: {e}")
+#
+# invalid_restaurant = Restaurant(
+#     name="A",
+#     location="A" * 201,
+#     description="A restaurant with a long name and invalid rating.",
+#     rating=5.01,
+# )
+#
+# try:
+#     invalid_restaurant.full_clean()
+#     invalid_restaurant.save()
+#     print("Invalid Restaurant saved successfully!")
+# except Exception as e:
+#     print(f"Validation Error: {e}")
+# -------------------------------------------------------------------------------
+# Task 2:
