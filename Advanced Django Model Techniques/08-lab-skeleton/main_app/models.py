bbc07a3e6fe3a8fb824abc57533ea174f23345c2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-10 16:33:01 +0200

    Changes Commited

--- Advanced Django Model Techniques/08-lab-skeleton/main_app/models.py
+++ Advanced Django Model Techniques/08-lab-skeleton/main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
+from main_app.validators import validate_menu_categories
 
 
 # Create your models here.
@@ -7,13 +8,17 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator, MinVa
 class Restaurant(models.Model):
     name = models.CharField(
         max_length=100,
-        validators=[MinLengthValidator(2, message="Name must be at least 2 characters long."),
-                    MaxLengthValidator(100, message="Name cannot exceed 100 characters.")]
+        validators=[
+            MinLengthValidator(2, message="Name must be at least 2 characters long."),
+            MaxLengthValidator(100, message="Name cannot exceed 100 characters.")
+        ]
     )
     location = models.CharField(
         max_length=200,
-        validators=[MinLengthValidator(2, message="Location must be at least 2 characters long."),
-                    MaxLengthValidator(200, message="Location cannot exceed 200 characters.")]
+        validators=[
+            MinLengthValidator(2, message="Location must be at least 2 characters long."),
+            MaxLengthValidator(200, message="Location cannot exceed 200 characters.")
+        ]
     )
     description = models.TextField(
         null=True,
@@ -22,6 +27,22 @@ class Restaurant(models.Model):
     rating = models.DecimalField(
         max_digits=3,
         decimal_places=2,
-        validators=[MinValueValidator(0, message="Rating must be at least 0.00."),
-                    MaxValueValidator(5, message="Rating cannot exceed 5.00.")]
+        validators=[
+            MinValueValidator(0, message="Rating must be at least 0.00."),
+            MaxValueValidator(5, message="Rating cannot exceed 5.00.")
+        ]
     )
+
+
+class Menu(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    description = models.TextField(validators=[
+        validate_menu_categories
+    ])
+    restaurant = models.ForeignKey(
+        to=Restaurant,
+        on_delete=models.CASCADE
+    )
+
