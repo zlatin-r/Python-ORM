0f7da74b6ee67725bf4daf2bbf3ff757e5431e68
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-12 16:31:08 +0200

    Commiting Changes

--- Advanced Django Model Techniques - Exercise/08-exercise-skeleton/caller.py
+++ Advanced Django Model Techniques - Exercise/08-exercise-skeleton/caller.py
@@ -2,13 +2,12 @@ import os
 import django
 from django.core.exceptions import ValidationError
 
-
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Customer
+from main_app.models import Customer, Book
 
 # Create queries within functions
 
@@ -26,3 +25,20 @@ from main_app.models import Customer
 #     print('\n'.join(e.messages))
 # ---------------------------------------------------------------
 # Task 2:
+# book = Book(
+#     title="Short Title",
+#     description="A book with a short title.",
+#     genre="Fiction",
+#     author="A",
+#     isbn="1234")
+#
+# try:
+#     book.full_clean()
+#     book.save()
+# except ValidationError as e:
+#     print("Validation Error for Book:")
+#
+#     for field, errors in e.message_dict.items():
+#         print(f"{field}: {', '.join(errors)}")
+# -----------------------------------------------------
+# Task 3:
--- Advanced Django Model Techniques - Exercise/08-exercise-skeleton/main_app/models.py
+++ Advanced Django Model Techniques - Exercise/08-exercise-skeleton/main_app/models.py
@@ -41,70 +41,66 @@ class Customer(models.Model):
 
 class BaseMedia(models.Model):
     class Meta:
-        abstract = True,
+        abstract = True
         ordering = ["-created_at", "title"]
 
     title = models.CharField(
-        max_length=100
+        max_length=100,
     )
+
     description = models.TextField()
 
     genre = models.CharField(
-        max_length=50
+        max_length=50,
     )
+
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
 
 
 class Book(BaseMedia):
     class Meta(BaseMedia.Meta):
-        verbose_name = "Model Book",
+        verbose_name = "Model Book"
         verbose_name_plural = "Models of type - Book"
 
     author = models.CharField(
         max_length=100,
         validators=[
-            MinLengthValidator(
-                5,
-                message="Author must be at least 5 characters long")
+            MinLengthValidator(5, "Author must be at least 5 characters long"),
         ]
     )
+
     isbn = models.CharField(
         max_length=20,
         unique=True,
         validators=[
-            MinLengthValidator(
-                6,
-                message="ISBN must be at least 6 characters long")
+            MinLengthValidator(6, "ISBN must be at least 6 characters long"),
         ]
     )
 
 
 class Movie(BaseMedia):
     class Meta(BaseMedia.Meta):
-        verbose_name = "Model Movie",
+        verbose_name = "Model Movie"
         verbose_name_plural = "Models of type - Movie"
 
     director = models.CharField(
         max_length=100,
         validators=[
-            MinLengthValidator(
-                8,
-                message="Director must be at least 8 characters long")
+            MinLengthValidator(8, "Director must be at least 8 characters long"),
         ]
     )
 
 
 class Music(BaseMedia):
     class Meta(BaseMedia.Meta):
-        verbose_name = "Model Music",
+        verbose_name = "Model Music"
         verbose_name_plural = "Models of type - Music"
 
     artist = models.CharField(
         max_length=100,
-        validators=[MinLengthValidator(
-            9,
-            message="Artist must be at least 9 characters long")
+        validators=[
+            MinLengthValidator(9, "Artist must be at least 9 characters long"),
         ]
     )
