f127349d8d2854048795ca7069d63171ca8d6740
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-12 17:56:01 +0200

    Changes Commited

--- Advanced Django Model Techniques - Exercise/08-exercise-skeleton/caller.py
+++ Advanced Django Model Techniques - Exercise/08-exercise-skeleton/caller.py
@@ -2,6 +2,7 @@ import os
 from decimal import Decimal
 
 import django
+from django.contrib.postgres.search import SearchVector
 from django.core.exceptions import ValidationError
 
 # Set up Django
@@ -9,7 +10,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Customer, Book, Product, DiscountedProduct, SpiderHero, FlashHero
+from main_app.models import Customer, Book, Product, DiscountedProduct, SpiderHero, FlashHero, Document
 
 # Create queries within functions
 
@@ -113,3 +114,23 @@ from main_app.models import Customer, Book, Product, DiscountedProduct, SpiderHe
 # # Now you can check the updated energy levels
 # print(f"{spiderman.name} - Energy: {spiderman.energy}")
 # print(f"{flash.name} - Energy: {flash.energy}")
+# ---------------------------------------------------------------------
+# Task 5:
+#
+# # Create the first 'Document' object with a title and content.
+# document1 = Document.objects.create(
+#     title="Django Framework 1",
+#     content="Django is a high-level Python web framework for building web applications.",)
+# # Create the second 'Document' object with a title and content.
+# document2 = Document.objects.create(
+#     title="Django Framework 2",
+#     content="Django framework provides tools for creating web pages, handling URL routing, and more.",)
+# # Update the 'search_vector' field in the 'Document' model with search vectors.
+# Document.objects.update(
+#     search_vector=SearchVector('title', 'content'))
+# # Perform a full-text search for documents containing the words 'django' and 'web framework'.
+# results = Document.objects.filter(
+#     search_vector='django web framework')
+# # Print the search results.
+# for result in results:
+#     print(f"Title: {result.title}")
