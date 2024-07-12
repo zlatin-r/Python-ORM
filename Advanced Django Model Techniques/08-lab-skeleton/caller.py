4eda51ce6761210fa043dcd12003e99cd84b6b4d
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-12 10:39:01 +0200

    Changes Commited

--- Advanced Django Model Techniques/08-lab-skeleton/caller.py
+++ Advanced Django Model Techniques/08-lab-skeleton/caller.py
@@ -6,8 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-# from main_app.models import Restaurant, Menu
-# from django.core.exceptions import ValidationError
+from main_app.models import Restaurant, Menu, RestaurantReview
+from django.core.exceptions import ValidationError
 
 #
 # # Create queries within functions
@@ -69,3 +69,31 @@ django.setup()
 #     print("Invalid Menu saved successfully!")
 # except ValidationError as e:
 #     print(f"Validation Error: {e}")
+# ----------------------------------------------------------------------
+# Task 3:
+#
+# restaurant1 = Restaurant.objects.create(name="Restaurant A", location="123 Main St.", description="A cozy restaurant",
+#                                         rating=4.88)
+# restaurant2 = Restaurant.objects.create(name="Restaurant B", location="456 Elm St.", description="Charming restaurant",
+#                                         rating=3.59)
+#
+# RestaurantReview.objects.create(reviewer_name="Bob", restaurant=restaurant1, review_content="Good experience overall.",
+#                                 rating=4)
+# RestaurantReview.objects.create(reviewer_name="Aleks", restaurant=restaurant1, review_content="Great food and service!",
+#                                 rating=5)
+# RestaurantReview.objects.create(reviewer_name="Charlie", restaurant=restaurant2, review_content="It was ok!", rating=2)
+#
+# duplicate_review = RestaurantReview(reviewer_name="Aleks", restaurant=restaurant1, review_content="Another great meal!",
+#                                     rating=5)
+#
+# try:
+#     duplicate_review.full_clean()
+#     duplicate_review.save()
+# except ValidationError as e:
+#     print(f"Validation Error: {e}")
+#
+# print("All Restaurant Reviews:")
+# for review in RestaurantReview.objects.all():
+#     print(f"Reviewer: {review.reviewer_name}, Rating: {review.rating}, Restaurant: {review.restaurant.name}")
+# -----------------------------------------------------------------------------------------------------------------------
+# Task 3:
