da4b234fc08f02e3ef3b4acf143032a3e0ac37d7
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-13 14:54:18 +0200

    Changes Commited

--- Advanced Queries in Django/09-lab-skeleton/caller.py
+++ Advanced Queries in Django/09-lab-skeleton/caller.py
@@ -1,5 +1,6 @@
 import os
 import django
+from django.db.models import Sum
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -11,11 +12,27 @@ from main_app.models import Product, Category, Customer, Order, OrderProduct
 
 # Create and run queries
 # Task 1:
-print('All Products:')
-print(Product.objects.all())
-print()
-print('All Available Products:')
-print(Product.objects.available_products())
-print()
-print('All Available Food Products:')
-print(Product.objects.available_products_in_category("Food"))
+#
+# print('All Products:')
+# print(Product.objects.all())
+# print()
+# print('All Available Products:')
+# print(Product.objects.available_products())
+# print()
+# print('All Available Food Products:')
+# print(Product.objects.available_products_in_category("Food"))
+# --------------------------------------------------------------------
+# Task 2:
+
+def product_quantity_ordered():
+    result = []
+
+    orders = Product.objects.annotate(
+        total=Sum("orderproduct__quantity")
+    ).values("name", "total"
+             ).order_by("total")
+
+    for order in orders:
+        result.append(f"Quantity ordered of {order["name"]}: {orders["total"]}")
+
+    return "\n".join(result)
