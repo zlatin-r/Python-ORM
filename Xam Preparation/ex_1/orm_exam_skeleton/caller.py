aa9da89a535906002894de3af58c6d0a56496144
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-21 09:12:29 +0200

    Changes Commited

--- Xam Preparation/ex_1/orm_exam_skeleton/caller.py
+++ Xam Preparation/ex_1/orm_exam_skeleton/caller.py
@@ -1,10 +1,16 @@
 import os
 import django
+from django.db.models import Q
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
+
 # Import your models here
 
 # Create queries within functions
+
+def get_directors(search_name=None, search_nationality=None):
+    if Q(search_name is None) & Q(search_nationality is None):
+        return ""
