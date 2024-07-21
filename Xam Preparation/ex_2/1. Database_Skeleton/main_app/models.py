b17712aa617f8e0f2ce992c0c3837d81284153e4
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-21 12:20:27 +0200

    Changes Commited

--- Xam Preparation/ex_2/1. Database_Skeleton/main_app/models.py
+++ Xam Preparation/ex_2/1. Database_Skeleton/main_app/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
+from main_app.mixins import CreationDateMixin
+
 
 # Create your models here.
+
+class Profile(CreationDateMixin):
+    full_name = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    in_stock = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    is_available = models.BooleanField(default=True)
+
+
+class Order(CreationDateMixin):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_completed = models.BooleanField(default=False)
