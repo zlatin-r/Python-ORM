ac1ac3c970c01c7ad160ba1e5e8dc11e782441b1
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-21 12:33:51 +0200

    Changes Commited

--- Xam Preparation/ex_2/1. Database_Skeleton/main_app/models.py
+++ Xam Preparation/ex_2/1. Database_Skeleton/main_app/models.py
@@ -6,7 +6,8 @@ from main_app.mixins import CreationDateMixin
 # Create your models here.
 
 class Profile(CreationDateMixin):
-    full_name = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    full_name = models.CharField(max_length=100,
+                                 validators=[MinLengthValidator(2), ])
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
     address = models.TextField()
@@ -17,7 +18,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
-    in_stock = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    in_stock = models.PositiveIntegerField()
     is_available = models.BooleanField(default=True)
 
 
