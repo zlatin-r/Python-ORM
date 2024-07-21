b4b14712156ea70ca634a829ad1cbb0a19578531
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-21 12:55:33 +0200

    Changes Commited

--- Xam Preparation/ex_2/1. Database_Skeleton/main_app/mixins.py
+++ Xam Preparation/ex_2/1. Database_Skeleton/main_app/mixins.py
@@ -1,4 +1,3 @@
-from django.core.validators import MinValueValidator
 from django.db import models
 
 
--- Xam Preparation/ex_2/1. Database_Skeleton/main_app/models.py
+++ Xam Preparation/ex_2/1. Database_Skeleton/main_app/models.py
@@ -6,19 +6,18 @@ from main_app.mixins import CreationDateMixin
 # Create your models here.
 
 class Profile(CreationDateMixin):
-    full_name = models.CharField(max_length=100,
-                                 validators=[MinLengthValidator(2), ])
+    full_name = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
     address = models.TextField()
     is_active = models.BooleanField(default=True)
 
 
-class Product(models.Model):
+class Product(CreationDateMixin):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
-    in_stock = models.PositiveIntegerField()
+    in_stock = models.PositiveIntegerField(validators=[MinValueValidator(0)])
     is_available = models.BooleanField(default=True)
 
 
@@ -27,3 +26,4 @@ class Order(CreationDateMixin):
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
     is_completed = models.BooleanField(default=False)
+
