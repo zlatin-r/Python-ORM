cd15937d423cb432f5d28a0dbd78c3583788a3ef
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-21 12:28:27 +0200

    Changes Commited

--- Xam Preparation/ex_2/1. Database_Skeleton/main_app/mixins.py
+++ Xam Preparation/ex_2/1. Database_Skeleton/main_app/mixins.py
@@ -7,10 +7,3 @@ class CreationDateMixin(models.Model):
 
     class Meta:
         abstract = True
-
-
-class TotalPriceMixin(models.Model):
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
-
-    class Meta:
-        abstract = True
