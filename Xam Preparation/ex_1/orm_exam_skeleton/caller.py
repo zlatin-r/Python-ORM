3a16fff4c65ffa0772f9472a3054e9916d5fde6c
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-21 09:51:05 +0200

    Changes Commited

--- Xam Preparation/ex_1/orm_exam_skeleton/caller.py
+++ Xam Preparation/ex_1/orm_exam_skeleton/caller.py
@@ -1,6 +1,6 @@
 import os
 import django
-from django.db.models import Q
+from django.db.models import Q, Count, Avg
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -8,7 +8,7 @@ django.setup()
 
 # Import your models here
 
-from main_app.models import Director
+from main_app.models import Director, Actor
 
 
 # Create queries within functions
@@ -23,6 +23,35 @@ def get_directors(search_name=None, search_nationality=None):
     if search_name is not None and search_nationality is not None:
         query = Q(query_name & query_nationality)
     elif search_name is not None:
-        query = Q(query_name)
+        query = query_name
     else:
-        query = Q(query_nationality)
+        query = query_nationality
+
+    directors = Director.objects.filter(query).order_by('full_name')
+
+    if not directors:
+        return ""
+
+    result = []
+
+    for d in directors:
+        result.append(f"Director: {d.full_name}, nationality: {d.nationality}, experience: {d.years_of_experience}")
+
+    return "\n".join(result)
+
+
+def get_top_director():
+    director = Director.objects.get_directors_by_movies_count().first()
+
+    if not director:
+        return ""
+
+    return f"Top Director: {director.full_name}, movies: {director.movies_count}"
+
+
+def get_top_actor():
+    actor = Actor.objects.prefetch_related("starring_movies").annotate(
+        movie_count=Count("starring_movies"),
+        avg_rating=Avg("starring_movies__rating")
+    ).order_by("-movie_count", "full_name").first()
+
