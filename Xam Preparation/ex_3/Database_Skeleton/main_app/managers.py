283a89941e830b7b17655307e4fde4045f1bcd9a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-24 10:47:17 +0200

    Changes Commited

--- Xam Preparation/ex_3/Database_Skeleton/main_app/managers.py
+++ Xam Preparation/ex_3/Database_Skeleton/main_app/managers.py
@@ -4,4 +4,4 @@ from django.db.models import Count
 
 class AuthorManager(models.Manager):
     def get_authors_by_article_count(self):
-        return self.annotate(article_count=Count('articles')).order_by('-article_count', 'email')
+        return self.annotate(article_count=Count('article')).order_by('-article_count', 'email')
