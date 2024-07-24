919fa7fd12611b1c2e14817f5aef158682a6ae17
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-24 09:37:42 +0200

    Changes Commited

--- Xam Preparation/ex_3/Database_Skeleton/main_app/admin.py
+++ Xam Preparation/ex_3/Database_Skeleton/main_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from main_app.models import Author, Article, Review
+
+
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
