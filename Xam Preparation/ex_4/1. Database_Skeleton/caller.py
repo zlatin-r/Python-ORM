2360ba0dab4b2a866db3e96e17d516a8592ed1f6
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-25 09:47:38 +0200

    Changes Commited

--- Xam Preparation/ex_4/1. Database_Skeleton/caller.py
+++ Xam Preparation/ex_4/1. Database_Skeleton/caller.py
@@ -8,4 +8,28 @@ django.setup()
 # Import your models here
 from main_app.models import TennisPlayer
 
+
 # Create queries within functions
+
+def get_tennis_players(search_name=None, search_country=None):
+    players = None
+
+    if not search_name and not search_country:
+        return ""
+    elif search_name and search_country:
+        players = TennisPlayer.objects.filter(full_name__icontains=search_name, country__icontains=search_country)
+    elif search_name and not search_country:
+        players = TennisPlayer.objects.filter(full_name__icontains=search_name)
+    elif not search_name and search_country:
+        players = TennisPlayer.objects.filter(country__icontains=search_country)
+
+    if not players:
+        return ""
+
+    result = [f"Tennis Player: {p.full_name}, country: {p.country}, ranking: {p.ranking}"
+              for p in players.order_by("ranking")]
+
+    return "\n".join(result)
+
+
+# print(get_tennis_players("Player 1", "Player 1"))
