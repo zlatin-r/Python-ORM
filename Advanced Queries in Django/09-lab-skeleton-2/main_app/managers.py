707dad50fd6bee2c22bb195abc34100081c08dc0
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-27 09:47:19 +0200

    Changes Commited

--- Advanced Queries in Django/09-lab-skeleton-2/main_app/managers.py
+++ Advanced Queries in Django/09-lab-skeleton-2/main_app/managers.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class ProductManager(models.Manager):
     def available_products(self):
-        return self.all().filter(is_available=True)
+        return self.filter(is_available=True)
 
     def available_products_in_category(self, category_name: str):
         return self.available_products().filter(category__name=category_name)
+             # self.filter(is_available=True, category__name=category_name)
