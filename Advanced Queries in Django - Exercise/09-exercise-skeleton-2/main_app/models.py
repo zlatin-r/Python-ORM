1fe1c8a2213fe430c3767b92d83f42d176bd5fe5
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-28 12:04:05 +0200

    Changes Commited

--- Advanced Queries in Django - Exercise/09-exercise-skeleton-2/main_app/models.py
+++ Advanced Queries in Django - Exercise/09-exercise-skeleton-2/main_app/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 from main_app.managers import RealEstateListingManager
@@ -35,7 +36,8 @@ class VideoGame(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
     release_year = models.PositiveIntegerField()
-    rating = models.DecimalField(max_digits=2,decimal_places=1)
+    rating = models.DecimalField(max_digits=2,decimal_places=1,
+                                 validators=[MinValueValidator(1), MaxValueValidator(5)], "The rating must be between 0.0 and 10.0")
 
     def __str__(self):
         return self.title
