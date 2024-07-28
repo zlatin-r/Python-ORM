e9d25077a48edeb61a263f9295e48bea2da331f9
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-28 15:46:06 +0200

    Changes Commited

--- Advanced Queries in Django - Exercise/09-exercise-skeleton-2/main_app/models.py
+++ Advanced Queries in Django - Exercise/09-exercise-skeleton-2/main_app/models.py
@@ -60,6 +60,19 @@ class Invoice(models.Model):
     invoice_number = models.CharField(max_length=20, unique=True)
     billing_info = models.OneToOneField(BillingInfo, on_delete=models.CASCADE)
 
+    @classmethod
+    def get_invoices_with_prefix(cls, prefix: str):
+        return cls.objects.filter(invoice_number__startswith=prefix)
+
+    @classmethod
+    def get_invoices_sorted_by_number(cls):
+        return cls.objects.order_by("invoice_number")
+
+    @classmethod
+    def get_invoice_with_billing_info(cls, invoice_number: str):
+        return cls.objects.get(invoice_number=invoice_number)
+        # cls.objects.select_related('billing_info').get(invoice_number=invoice_number)
+
 
 class Technology(models.Model):
     name = models.CharField(max_length=100)
@@ -71,10 +84,16 @@ class Project(models.Model):
     description = models.TextField()
     technologies_used = models.ManyToManyField(Technology, related_name='projects')
 
+    def get_programmers_with_technologies(self):
+        return self.programmers.prefetch_related("projects__technologies_used")
+
 
 class Programmer(models.Model):
     name = models.CharField(max_length=100)
     projects = models.ManyToManyField(Project, related_name='programmers')
+
+    def get_projects_with_technologies(self):
+        return self.projects.prefetch_related("technologies_used")
 
 
 class Task(models.Model):
