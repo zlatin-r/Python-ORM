e52e88839699bccd6f7d1b61ef23c78baba6f7bf
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-29 10:45:39 +0200

    Changes Commited

---
+++ Xam Preparation/ex_1.2/orm_exam_skeleton/main_app/mixins.py
@@ -0,0 +1,16 @@
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+class PersonInfoMixin(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
+    birth_date = models.DateField(default="1900-01-01")
+    nationality = models.CharField(max_length=50, default="Unknown")
+
+
+class IsAwarded(models.Model):
+    is_awarded = models.BooleanField(default=False)
+
+
+class LastUpdated(models.Model):
+    last_updated = models.DateTimeField(auto_now=True)
--- Xam Preparation/ex_1.2/orm_exam_skeleton/main_app/models.py
+++ Xam Preparation/ex_1.2/orm_exam_skeleton/main_app/models.py
@@ -1,3 +1,33 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
+from main_app.mixins import PersonInfoMixin, IsAwarded, LastUpdated
+
+
 # Create your models here.
+
+class Director(PersonInfoMixin):
+    years_of_experience = models.SmallIntegerField(validators=[MinValueValidator(0)], default=0)
+
+
+class Actor(PersonInfoMixin, IsAwarded, LastUpdated):
+    pass
+
+
+class Movie(IsAwarded, LastUpdated):
+    class GenreChoices(models.TextChoices):
+        ACTION = "Action", "Action"
+        COMEDY = "Comedy", "Comedy"
+        DRAMA = "Drama", "Drama"
+        OTHER = "Other", "Other"
+
+    title = models.CharField(max_length=150, validators=[MinLengthValidator(5)])
+    release_date = models.DateField()
+    storyline = models.TextField(null=True, blank=True)
+    genre = models.CharField(max_length=6, choices=GenreChoices, default=GenreChoices.OTHER)
+    rating = models.DecimalField(max_digits=3, decimal_places=1,
+                                 validators=[MinValueValidator(0), MaxValueValidator(10)])
+    is_classic = models.BooleanField(default=False)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    starring_actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, null=True, blank=True)
+    actors = models.ManyToManyField(Actor)
