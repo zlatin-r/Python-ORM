a810ed78e664ef4ac84161dc9eaf8d07381edac9
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-29 16:45:11 +0200

    Changes Commited

--- Xam Preparation/ex_2.2/1. Database_Skeleton/caller.py
+++ Xam Preparation/ex_2.2/1. Database_Skeleton/caller.py
@@ -1,6 +1,6 @@
 import os
 import django
-from django.db.models import Q
+from django.db.models import Q, Count, F
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -18,9 +18,9 @@ def get_profiles(search_string=None) -> str:
 
     profiles = Profile.objects.filter(
         Q(full_name__icontains=search_string)
-            |
+        |
         Q(email__icontains=search_string)
-            |
+        |
         Q(phone_number__icontains=search_string)
     ).order_by('full_name')
 
@@ -55,3 +55,29 @@ def get_last_sold_products():
     products = ', '.join(latest_order.products.order_by('name').values_list('name', flat=True))
 
     return f"Last sold products: {products}"
+
+
+def get_top_products():
+    top_products = Product.objects.annotate(count_orders=Count("orders")) \
+        .filter(count_orders__gt=0) \
+        .order_by('-count_orders', 'name')[:5]
+
+    if not top_products.exists():
+        return ""
+
+    result = "\n".join(f"{p.name}, sold {p.count_orders} times" for p in top_products)
+
+    return f"Top products:\n" + result
+
+
+def apply_discounts() -> str:
+    updated_orders_count = Order.objects.annotate(
+        products_count=Count('products')
+    ).filter(
+        products_count__gt=2,
+        is_completed=False
+    ).update(
+        total_price=F('total_price') * 0.90
+    )
+
+    return f"Discount applied to {updated_orders_count} orders."
