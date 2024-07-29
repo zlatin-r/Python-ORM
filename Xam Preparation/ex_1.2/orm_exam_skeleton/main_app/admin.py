fdd0e5c0b771942b96e9c5f61435128a04cd5ee6
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-29 11:00:32 +0200

    Changes Commited

--- Xam Preparation/ex_1.2/orm_exam_skeleton/main_app/admin.py
+++ Xam Preparation/ex_1.2/orm_exam_skeleton/main_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from main_app.models import Actor, Director, Movie
+
+
 # Register your models here.
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date', 'nationality')
+    list_filter = ('years_of_experience',)
+    search_fields = ('full_name', 'nationality')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
