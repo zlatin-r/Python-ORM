51433fd3be8e242e98b5f3ac5382dd79c3f918e0
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-29 11:32:15 +0200

    Changes Commited

--- Xam Preparation/ex_1.2/orm_exam_skeleton/caller.py
+++ Xam Preparation/ex_1.2/orm_exam_skeleton/caller.py
@@ -7,9 +7,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from poulate import populate_model_with_data
-from main_app.models import Director, Actor, Movie
 
 # Create queries within functions
-
-populate_model_with_data(Director)
