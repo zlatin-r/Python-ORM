bd159a24c1238c289916d62d58a5ab9d5ec708d8
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-29 15:04:21 +0200

    Changes Commited

--- Xam Preparation/ex_2.2/1. Database_Skeleton/main_app/models.py
+++ Xam Preparation/ex_2.2/1. Database_Skeleton/main_app/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MaxLengthValidator, MinLengthValidator, MinValueValidator
 from django.db import models
 
+from main_app.mixins import TimeStampMixin
+
+
 # Create your models here.
+
+class Profile(TimeStampMixin):
+    full_name = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+
+class Product(TimeStampMixin):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                validators=[MinValueValidator(0, 0.01)])
+    in_stock = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    is_available = models.BooleanField(default=True)
+
+
+class Order(TimeStampMixin):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    is_completed = models.BooleanField(default=False)
