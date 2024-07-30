69558d62025fbc0dec5984240852916dae0a7a72
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-30 10:02:51 +0200

    Changes Commited

---
+++ Xam Preparation/ex_3.2/1. Database_Skeleton/main_app/mixins.py
@@ -0,0 +1,16 @@
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+class PublishedMixin(models.Model):
+    published_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class ContentMixin(models.Model):
+    content = models.TextField(validators=[MinLengthValidator(10)])
+
+    class Meta:
+        abstract = True
--- Xam Preparation/ex_3.2/1. Database_Skeleton/main_app/models.py
+++ Xam Preparation/ex_3.2/1. Database_Skeleton/main_app/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
+from main_app.mixins import PublishedMixin, ContentMixin
+
+
 # Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    email = models.EmailField(unique=True)
+    is_banned = models.BooleanField(default=False)
+    birth_year = models.PositiveIntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2005)])
+    web_site = models.URLField(null=True, blank=True)
+
+
+class Article(PublishedMixin, ContentMixin):
+    class CategoryChoices(models.TextChoices):
+        TECHNOLOGY = 'Technology', 'Technology'
+        SCIENCE = 'Science', 'Science'
+        EDUCATION = 'Education', 'Education'
+
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(5)])
+    category = models.CharField(max_length=10, choices=CategoryChoices, default=CategoryChoices.TECHNOLOGY)
+    authors = models.ManyToManyField(Author)
+
+
+class Review(PublishedMixin, ContentMixin):
+    rating = models.FloatField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='reviews')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='reviews')
