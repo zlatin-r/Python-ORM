0d7ccc7f14cb2b54c4f55cbc98c887fe9cfc6ab1
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-30 14:27:13 +0200

    Initial Commit

--- Xam Preparation/ex_4/1. Database_Author's Solution/main_app/custom_model_manager.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class TennisPlayerManager(models.Manager):
-    def get_tennis_players_by_wins_count(self):
-        return self.annotate(wins_count=models.Count('matches_won')).order_by('-wins_count', 'full_name')
