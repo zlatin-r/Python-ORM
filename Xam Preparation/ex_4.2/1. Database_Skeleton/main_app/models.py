d6ace9f8da11a690f1bf6d8fc6aa1115e728f662
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-30 14:45:23 +0200

    Initial Commit

--- Xam Preparation/ex_4.2/1. Database_Skeleton/main_app/models.py
+++ Xam Preparation/ex_4.2/1. Database_Skeleton/main_app/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
+from main_app.managers import TennisPlayerManager
+
 
 # Create your models here.
 
@@ -11,6 +13,8 @@ class TennisPlayer(models.Model):
     ranking = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(300)])
     is_active = models.BooleanField(default=True)
 
+    objects = TennisPlayerManager()
+
 
 class Tournament(models.Model):
     class SurfaceChoices(models.TextChoices):
@@ -18,6 +22,7 @@ class Tournament(models.Model):
         CLAY = "Clay", "Clay"
         GRASS = "Grass", "Grass"
         HARD_COURT = "Hard Court", "Hard Court"
+
     name = models.CharField(max_length=150, validators=[MinLengthValidator(2)], unique=True)
     location = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
     prize_money = models.DecimalField(max_digits=10, decimal_places=2)
