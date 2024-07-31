dc6add6728159158f9b64cc9d774f1a11a6cc8ab
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-31 12:11:44 +0200

    Changes Commited

--- Xam Preparation/ex_2.3/1. Database_Skeleton/caller.py
+++ Xam Preparation/ex_2.3/1. Database_Skeleton/caller.py
@@ -1,5 +1,6 @@
 import os
 import django
+from django.db.models import Q
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -11,5 +12,45 @@ from main_app.models import Profile, Product, Order
 
 # Create queries within functions
 
+def get_profiles(search_string=None):
+    if not search_string:
+        return ""
 
-# print(Profile.objects.get_regular_customers())
+    query = Q(full_name__icontains=search_string) \
+            | Q(email__icontains=search_string) \
+            | Q(phone_number__icontains=search_string)
+
+    profiles = Profile.objects.filter(query).order_by('full_name')
+
+    if not profiles.exists():
+        return ""
+
+    result = [(f"Profile: {p.full_name}, "
+               f"email: {p.email}, "
+               f"phone number: {p.phone_number}, "
+               f"orders: {p.order_profiles.count()}")
+              for p in profiles]
+
+    return "\n".join(result)
+
+
+def get_loyal_profiles():
+    profiles = Profile.objects.get_regular_customers()
+
+    if not profiles:
+        return ""
+
+    result = [f"Profile: {p.full_name}, orders: {p.order_profiles.count()}" for p in profiles]
+
+    return "\n".join(result)
+
+
+def get_last_sold_products():
+    order = Order.objects.order_by("creation_date").last()
+
+    if not order or order.products.count() == 0:
+        return ""
+
+    products = ", ".join(p.name for p in order.products.all())
+
+    return f"Last sold products: {products}"
