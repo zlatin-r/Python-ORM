135254cf8fd57e269ab8a3644d56e9e89216c7c7
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-31 11:47:57 +0200

    Changes Commited

--- Xam Preparation/ex_2.3/1. Database_Skeleton/caller.py
+++ Xam Preparation/ex_2.3/1. Database_Skeleton/caller.py
@@ -6,5 +6,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Profile, Product, Order
+
 
 # Create queries within functions
+
+
+# print(Profile.objects.get_regular_customers())
--- Xam Preparation/ex_2.3/1. Database_Skeleton/main_app/managers.py
+++ Xam Preparation/ex_2.3/1. Database_Skeleton/main_app/managers.py
@@ -6,4 +6,4 @@ class ProfileManager(models.Manager):
     def get_regular_customers(self):
         return self.annotate(count_orders=Count('order_profiles')) \
             .filter(count_orders__gt=2) \
-            .order_by('.count_orders')
+            .order_by('-count_orders')
