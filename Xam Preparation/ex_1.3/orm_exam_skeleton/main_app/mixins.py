88cf547a28fe8efbaba8f4448bbe2b508ff03de0
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-07-31 09:36:08 +0200

    Changes Commited

--- Xam Preparation/ex_1.3/orm_exam_skeleton/main_app/mixins.py
+++ Xam Preparation/ex_1.3/orm_exam_skeleton/main_app/mixins.py
@@ -6,3 +6,20 @@ class PersonInfo(models.Model):
     full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
     birth_date = models.DateField(default="1900-01-01")
     nationality = models.CharField(max_length=50, default="Unknown")
+
+    class Meta:
+        abstract = True
+
+
+class LastUpdated(models.Model):
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class IsAwarded(models.Model):
+    is_awarded = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
--- Xam Preparation/ex_1.3/orm_exam_skeleton/main_app/models.py
+++ Xam Preparation/ex_1.3/orm_exam_skeleton/main_app/models.py
@@ -1,7 +1,7 @@
-from django.core.validators import MinLengthValidator, MinValueValidator
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-from main_app.mixins import PersonInfo
+from main_app.mixins import PersonInfo, LastUpdated, IsAwarded
 
 
 # Create your models here.
@@ -9,5 +9,24 @@ class Director(PersonInfo):
     years_of_experience = models.SmallIntegerField(validators=[MinValueValidator(0)], default=0)
 
 
-class Actor(PersonInfo):
+class Actor(PersonInfo, LastUpdated, IsAwarded):
     is_awarded = models.BooleanField(default=False)
+
+
+class Movie(IsAwarded, LastUpdated):
+    class GenreChoices(models.TextChoices):
+        ACTION = "Action", "Action"
+        COMEDY = "Comedy", "Comedy"
+        DRAMA = "Drama", "Drama"
+        OTHER = "Other", "Other"
+
+    title = models.CharField(max_length=150, validators=[MinLengthValidator(5)])
+    release_date = models.DateField()
+    storyline = models.TextField(null=True, blank=True)
+    genre = models.CharField(choices=GenreChoices, max_length=6, default=GenreChoices.OTHER)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, default=0,
+                                 validators=[MinValueValidator(0), MaxValueValidator(10)])
+    is_classical = models.BooleanField(default=False)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='directors')
+    starring_actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, null=True, related_name='starring_actors')
+    actors = models.ManyToManyField(Actor, related_name='actors')
