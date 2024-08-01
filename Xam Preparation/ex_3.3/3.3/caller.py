f23b96dcad064ebdaf8a45b21e3562058d465000
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-01 09:45:50 +0200

    Initial Commit

--- Xam Preparation/ex_3.3/3.3/caller.py
+++ Xam Preparation/ex_3.3/3.3/caller.py
@@ -5,6 +5,20 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
+
 # Import your models here
 
 # Create queries within functions
+
+def get_authors(search_name=None, search_email=None):
+    pass
+
+
+def get_top_publisher():
+    pass
+
+
+def get_top_reviewer():
+    pass
+
+
