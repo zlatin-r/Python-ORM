aa91b938721aec290b55ca40d504cafcfe832177
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-01 15:56:02 +0200

    Changes Commited

--- Xam Preparation/ex_4.3/4.3/caller.py
+++ Xam Preparation/ex_4.3/4.3/caller.py
@@ -22,7 +22,7 @@ def get_tennis_players(search_name=None, search_country=None):
         query = Q(full_name__icontains=search_name) & Q(country__icontains=search_country)
     elif search_name and not search_country:
         query = Q(full_name__icontains=search_name)
-    elif not search_country and search_name:
+    elif not search_name and search_country:
         query = Q(country__icontains=search_country)
 
     players = TennisPlayer.objects.filter(query).order_by("ranking")
@@ -38,7 +38,7 @@ def get_tennis_players(search_name=None, search_country=None):
 def get_top_tennis_player():
     player = TennisPlayer.objects.get_tennis_players_by_wins_count().first()
 
-    if not player or player.count_wins == 0:
+    if not player:
         return ""
 
     return f"Top Tennis Player: {player.full_name} with {player.count_wins} wins."
@@ -51,3 +51,20 @@ def get_tennis_player_by_matches_count():
         return ""
 
     return f"Tennis Player: {player.full_name} with {player.count_matches} matches played."
+
+
+def get_tournaments_by_surface_type(surface=None):
+    if not surface:
+        return ""
+
+    tournaments = Tournament.objects.annotate(count_matches=Count("matches")) \
+                                    .filter(surface_type__icontains=surface) \
+                                    .order_by("-start_date")
+
+    if not tournaments:
+        return ""
+
+    result = [f"Tournament: {t.name}, start date: {t.start_date}, matches: {s.count_matches}"
+              for t in tournaments]
+
+    return "\n".join(result)
