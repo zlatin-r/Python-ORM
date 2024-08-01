3a8d934b1dc5d9c16fabcc4574d04f21395482ce
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-01 15:17:43 +0200

    Initial Commit

--- Xam Preparation/ex_4.3/4.3/main_app/models.py
+++ Xam Preparation/ex_4.3/4.3/main_app/models.py
@@ -1,3 +1,40 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
+
+
+class TennisPlayer(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(5)])
+    birth_date = models.DateField()
+    country = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    ranking = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(300)])
+    is_active = models.BooleanField(default=True)
+
+    # objects = TennisPlayerManager()
+
+
+class Tournament(models.Model):
+    class SurfaceChoices(models.TextChoices):
+        NOT_SELECTED = "Not Selected", "Not Selected"
+        CLAY = "Clay", "Clay"
+        GRASS = "Grass", "Grass"
+        HARD_COURT = "Hard Court", "Hard Court"
+
+    name = models.CharField(max_length=150, validators=[MinLengthValidator(2)], unique=True)
+    location = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    prize_money = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    surface_type = models.CharField(max_length=12, choices=SurfaceChoices, default=SurfaceChoices.NOT_SELECTED)
+
+
+class Match(models.Model):
+    score = models.CharField(max_length=100)
+    summary = models.TextField(validators=[MinLengthValidator(5)])
+    date_played = models.DateTimeField()
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='matches')
+    players = models.ManyToManyField(TennisPlayer, related_name='matches')
+    winner = models.ForeignKey(TennisPlayer, on_delete=models.SET_NULL, null=True, related_name='winner')
+
+    class Meta:
+        verbose_name_plural = "Matches"
