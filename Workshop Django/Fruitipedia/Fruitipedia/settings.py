aa6daf36112a9794d0671da9eb74d0c52e47c2d1
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-04 11:09:43 +0200

    Initial Commit

--- Workshop Django/Fruitipedia/Fruitipedia/settings.py
+++ Workshop Django/Fruitipedia/Fruitipedia/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'Fruitipedia.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydatabase",
-        "USER": "mydatabaseuser",
-        "PASSWORD": "mypassword",
+        "NAME": "fruitipedia",
+        "USER": "postgres",
+        "PASSWORD": "password",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
