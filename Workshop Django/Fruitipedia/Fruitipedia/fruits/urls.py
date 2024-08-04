a9759c06d609828e1d68f11770d10f28a9fc6996
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-04 16:05:06 +0200

    Changes Commited

--- Workshop Django/Fruitipedia/Fruitipedia/fruits/urls.py
+++ Workshop Django/Fruitipedia/Fruitipedia/fruits/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
 from Fruitipedia.fruits import views
 
 urlpatterns = (
     path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('create-category/', views.create_category, name='create_category'),
+    path('create-fruit/', views.create_view, name='create-fruit'),
+    path('<int:pk>/', include([
+        path('edit-fruit/', views.edit_view, name='edit-fruit'),
+        path('delete-fruit/', views.delete_view, name='delete-fruit'),
+        path('details-fruit/', views.details_view, name='details-fruit'),
+    ]))
 )
