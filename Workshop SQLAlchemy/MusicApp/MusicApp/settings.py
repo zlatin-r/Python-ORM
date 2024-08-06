b0c19c8c670ac0623567352d87486be57ea46bfa
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-06 14:19:29 +0200

    Changes Commited

--- Workshop SQLAlchemy/MusicApp/MusicApp/settings.py
+++ Workshop SQLAlchemy/MusicApp/MusicApp/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'MusicApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "music_db",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
