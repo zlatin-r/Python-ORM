53ab915e1927ec59b5f60095c9920117eb2a732f
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-06 15:14:26 +0200

    Changes Commited

--- Workshop SQLAlchemy/MusicApp/MusicApp/musics/models.py
+++ Workshop SQLAlchemy/MusicApp/MusicApp/musics/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from sqlalchemy import Column, Integer, String, Float, relationship, ForeignKey
+
+from MusicApp.settings import Base
+
 
 # Create your models here.
+
+class Album(Base):
+    __tablename__ = 'albums'
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+    album_name = Column(
+        String(30),
+        nullable=False,
+        unique=True
+    )
+    image_url = Column(
+        String(255),
+        nullable=False
+    )
+    price = Column(
+        Float,
+        nullable=False
+    )
+    songs = relationship(
+        "Song",
+        back_populates="album",
+        cascade="all, delete-orphan"
+    )
+
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+    song_name = Column(
+        String(100),
+        nullable=False,
+    )
+    album_id = Column(
+        ForeignKey('albums.id'),
+        Integer,
+        nullable=False
+    )
+    album = relationship(
+        "Album",
+        back_populates="songs",
+    )
