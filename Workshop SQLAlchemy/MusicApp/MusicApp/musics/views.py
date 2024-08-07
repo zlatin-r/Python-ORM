3ed3ba0d17de703624fe5cef61777468d4c5a525
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-07 08:49:44 +0200

    Changes Commited

--- Workshop SQLAlchemy/MusicApp/MusicApp/musics/views.py
+++ Workshop SQLAlchemy/MusicApp/MusicApp/musics/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'common/index.html')
+
+
+def create_album(request):
+    return render(request, 'albums/create-album.html')
+
+
+def edit_album(request):
+    return render(request, 'albums/edit-album.html')
+
+
+def delete_album(request):
+    return render(request, 'albums/delete-album.html')
+
+
+def album_details(request):
+    return render(request, 'albums/album-details.html')
+
+
+def create_song(request):
+    return render(request, 'songs/create-song.html')
