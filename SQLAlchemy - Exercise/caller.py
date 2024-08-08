21dbd7512b7174f0765aa1d43b40bded99e1ee13
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-08 11:37:36 +0200

    Changes Commited

--- SQLAlchemy - Exercise/caller.py
+++ SQLAlchemy - Exercise/caller.py
@@ -24,3 +24,24 @@ def create_recipe(name: str, ingredients: str, instructions: str) -> None:
 
 for name, ingredient, instruction in recipes:
     create_recipe(name, ingredient, instruction)
+
+
+@session_decorator(session)
+def update_recipe_by_name(name: str, new_name: str, new_ingredients: str, new_instructions: str) -> int:
+    records_changed: int = (
+        session.query(Recipe)
+        .filter_by(name=name)
+        .update({
+            Recipe.name: new_name,
+            Recipe.ingredients: new_ingredients,
+            Recipe.instructions: new_instructions
+        })
+    )
+    # recipe_to_update = session.query(Recipe).filter_by(name=name).first()
+    #
+    # recipe_to_update.name = new_name
+    # recipe_to_update.ingredients = new_ingredients
+    # recipe_to_update.instructions = new_instructions
+
+    return records_changed
+
