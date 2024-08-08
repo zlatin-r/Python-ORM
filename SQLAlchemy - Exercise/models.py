144e3dbf2a054748a66a87fedae4c27d7ccd0848
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-08 10:14:04 +0200

    Changes Commited

--- SQLAlchemy - Exercise/models.py
+++ SQLAlchemy - Exercise/models.py
@@ -26,32 +26,3 @@ class Recipe(Base):
         Text,
         nullable=False
     )
-
-    chef_id = Column(
-        Integer,
-        ForeignKey('chefs.id')
-    )
-
-    chef = relationship(
-        "Chef",
-        back_populates="recipes",
-    )
-
-
-class Chef(Base):
-    __tablename__ = "chefs"
-
-    id = Column(
-        Integer,
-        primary_key=True
-    )
-
-    name = Column(
-        String(100),
-        nullable=False
-    )
-
-    recipes = relationship(
-        "Recipe",
-        back_populates="chef"
-    )
