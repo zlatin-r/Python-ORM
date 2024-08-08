e40e52f25c166e6a55ff29fd1da37d57ba4399fb
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-08-08 11:28:50 +0200

    Changes Commited

--- SQLAlchemy - Exercise/caller.py
+++ SQLAlchemy - Exercise/caller.py
@@ -1,7 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from helpers import session_decorator
+from models import Recipe
+from seed import recipes
+
 engine = create_engine("postgresql+psycopg2://postgres:password@localhost/sqlalchemy_db")
-Session = sessionmaker(engine=engine)
+Session = sessionmaker(bind=engine)
 
 session = Session()
+
+
+@session_decorator(session)
+def create_recipe(name: str, ingredients: str, instructions: str) -> None:
+    new_recipe = Recipe(
+        name=name,
+        ingredients=ingredients,
+        instructions=instructions
+    )
+
+    session.add(new_recipe)
+
+
+for name, ingredient, instruction in recipes:
+    create_recipe(name, ingredient, instruction)
